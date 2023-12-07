f98b96975020bad214848d112cd2a4fca0c159d8
unknown <waqasarif588@gmail.com>
2023-12-07 18:37:36 +0500

    changes 7 DEC

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-import express  from "express";
-import env from 'dotenv';
+import express from "express";
+import env from "dotenv";
 env.config();
 const app = express();
-import  color  from "colors";
+import color from "colors";
 import connectDB from "./db/connection.js";
 import morgan from "morgan";
+import authRoutes from "./routes/authRoutes.js";
 
 const port = process.env.PORT || 8080;
 const DB_URL = process.env.MONGO_URL;
 
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
+// api routes
+app.use('/api',authRoutes);
 connectDB(DB_URL);
 
-
-app.listen(port,() => {
-    console.log(`App is running on port ${port}`.white)
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`.white);
 });
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+import Jwt from "jsonwebtoken";
+import userModel from "../model/userModel.js";
+
+export const requireSigIn = (req, res, next) => {
+  try {
+    const decode = Jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.user = decode;
+    next();
+  } catch (error) {
+    return res.status(401).send({ succcess: false, message: error });
+  }
+};
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await userModel.findOne({ _id: user._id });
+    if (user.role != 1) {
+      return res
+        .status(401)
+        .send({ succcess: false, message: "Unauthorized Access" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    return res
+      .status(401)
+      .send({ succcess: false, message: "Not a admin", error });
+  }
+};

e9b8b89958d8853a51e47074c3a8cf15f7fd9ff3
waqas <waqasarif588@gmail.com>
2023-12-15 12:53:32 +0500

    reactrouter added

---
+++ client/src/pages/Notfound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Notfound = () => {
+  return (
+    <>
+      <Layout> Notfound</Layout>
+    </>
+  );
+};
+
+export default Notfound;
